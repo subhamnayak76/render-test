eb9ab411803ff33aea8ef3ed14fb26f211e85408
subhamnayak76 <subhamnayak9638@gmail.com>
2024-06-17 10:43:29 +0530

    fixed mongo.js issue

--- mongo.js
+++ mongo.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-const url = `mongodb+srv://subhamnayak9638:${password}@cluster0.b2awyto.mongodb.net/`
-mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
 
 mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
     name: String,
